ebba33badec5801414d7eb261da884adda7a73e8
Lakhan Gupta <2021pcecslakhan090@poornima.org>
2024-07-08 00:13:38 +0530

    work page added

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -1,9 +1,80 @@
-import React from 'react'
-
+import React from "react";
+import { FaExternalLinkAlt } from "react-icons/fa";
+import './work.css'
 const Work = () => {
   return (
-    <div>Work</div>
-  )
-}
+    <div className="workSection">
+      <div className="workLeft">
+        <div className="workheading"><span>W</span>ork</div>
+        <div className="workContent">POWERFUL COMPELLING EXPERIENCES</div>
+      </div>
+      <div className="workRight">
+        <div className="Project">
+          <div className="Projectimage">
+            <img src="./images/LegalYodha.jpg" alt="error" />
+          </div>
+          <div className="projectinfo">
+            <div className="Projectname">Legal Yodha</div>
+            <FaExternalLinkAlt className="projectLink"></FaExternalLinkAlt>
+          </div>
+        </div>
+        <div className="Project">
+          <div className="Projectimage">
+            <img src="./images/JSR.jpg" alt="error" />
+          </div>
+          <div className="projectinfo">
+            <div className="Projectname">JSR International</div>
+            <FaExternalLinkAlt className="projectLink"></FaExternalLinkAlt>
+          </div>
+        </div>
+        <div className="Project">
+          <div className="Projectimage">
+            <img src="./images/Rentellar.jpg" alt="error" />
+          </div>
+          <div className="projectinfo">
+            <div className="Projectname">Rentellar</div>
+            <FaExternalLinkAlt className="projectLink"></FaExternalLinkAlt>
+          </div>
+        </div>
+        <div className="Project">
+          <div className="Projectimage">
+            <img src="./images/AryanPortfoilio.jpg" alt="error" />
+          </div>
+          <div className="projectinfo">
+            <div className="Projectname">Portfolio</div>
+            <FaExternalLinkAlt className="projectLink"></FaExternalLinkAlt>
+          </div>
+        </div>
+        <div className="Project">
+          <div className="Projectimage">
+            <img src="./images/ChinmayPorfolio.jpg" alt="error" />
+          </div>
+          <div className="projectinfo">
+            <div className="Projectname">Porfolio</div>
+            <FaExternalLinkAlt className="projectLink"></FaExternalLinkAlt>
+          </div>
+        </div>
+        <div className="Project">
+          <div className="Projectimage">
+            <img src="./images/ClientPorfolio.jpg" alt="error" />
+          </div>
+          <div className="projectinfo">
+            <div className="Projectname">Porfolio</div>
+            <FaExternalLinkAlt className="projectLink"></FaExternalLinkAlt>
+          </div>
+        </div>
+        <div className="Project">
+          <div className="Projectimage">
+            <img src="./images/LakhanPorfolio.jpg" alt="error" />
+          </div>
+          <div className="projectinfo">
+            <div className="Projectname">Porfolio</div>
+            <FaExternalLinkAlt className="projectLink"></FaExternalLinkAlt>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Work
+export default Work;
