597dc2b4393f35b4996a12370932ddbdce62d12d
Lakhan Gupta <2021pcecslakhan090@poornima.org>
2024-07-09 23:05:47 +0530

    change

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { FaExternalLinkAlt } from "react-icons/fa";
+import { FaArrowRight } from "react-icons/fa";
+
 import './work.css'
 const Work = () => {
   return (
@@ -7,19 +9,11 @@ const Work = () => {
       <div className="workLeft">
         <div className="workheading"><span>W</span>ork</div>
         <div className="workContent">POWERFUL COMPELLING EXPERIENCES</div>
+        <div className="scroolheading"><p>Scrool to See </p><FaArrowRight /></div>
       </div>
      
 
       <div className="workRight">
-        <div className="Project">
-          <div className="Projectimage">
-            <img src="./images/LegalYodha.jpg" alt="error" />
-          </div>
-          <div className="projectinfo">
-            <div className="Projectname">Legal Yodha</div>
-            <FaExternalLinkAlt className="projectLink"></FaExternalLinkAlt>
-          </div>
-        </div>
         <div className="Project">
           <div className="Projectimage">
             <img src="./images/JSR.jpg" alt="error" />
@@ -35,6 +29,15 @@ const Work = () => {
           </div>
           <div className="projectinfo">
             <div className="Projectname">Rentellar</div>
+            <FaExternalLinkAlt className="projectLink"></FaExternalLinkAlt>
+          </div>
+        </div>
+        <div className="Project">
+          <div className="Projectimage">
+            <img src="./images/LegalYodha.jpg" alt="error" />
+          </div>
+          <div className="projectinfo">
+            <div className="Projectname">Legal Yodha</div>
             <FaExternalLinkAlt className="projectLink"></FaExternalLinkAlt>
           </div>
         </div>
